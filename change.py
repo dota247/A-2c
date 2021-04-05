b2f8afb207e4afbd8fa9677795d3acac4f2938c4
Wagner, J <35028309+dota247@users.noreply.github.com>
2021-04-05 09:23:58 -0700

    Update change.py

--- change.py
+++ change.py
@@ -2,9 +2,11 @@
 # Date  : 4/01/2021
 # Description: Script asks a user to enter an amount in cents less than a dollar
 
-# Using floor division you can find how many times a coin (25,10,5,1) fits evenly into the given number (user_input)
-# I use temp to hold whats left and then use floor division again with the coin value i want to find.
-# the value of temp will be adjusted 4 times.
+'''
+Using floor division you can find how many times a coin (25,10,5,1) fits evenly into the given number (user_input)
+I use temp to hold whats left and then use floor division again with the coin value i want to find.
+the value of temp will be adjusted 4 times.
+'''
 
 temp = 0
 
